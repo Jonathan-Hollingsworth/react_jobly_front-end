508edebc405e2a9a08847adfe6e2e54f1b2552b5
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-10-27 17:03:10 -0400

    Updated content for ./companies

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Route, Redirect, Switch} from 'react-router-dom'
+import CompanyList from './CompanyList'
 
 function Routes() {
     return(
         <Switch>
             <Route exact path='/companies/:handle'><h1>Coming soon</h1></Route>
-            <Route exact path='/companies'><h1>Coming soon</h1></Route>
+            <Route exact path='/companies'><h1><CompanyList /></h1></Route>
             <Route exact path='/jobs'><h1>Coming soon</h1></Route>
             <Route exact path='/profile'><h1>Coming soon</h1></Route>
             <Route exact path='/login'><h1>Coming soon</h1></Route>
